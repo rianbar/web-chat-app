3d25a18abbc9796d6d30e64761a04344aec8e871
rianbar <rianbarroso596@gmail.com>
2024-05-27 14:14:22 -0300

    setting home page path

---
+++ web-chat/src/main/java/com/rian/webchat/controller/PathController.java
@@ -0,0 +1,18 @@
+package com.rian.webchat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PathController {
+
+    @GetMapping("/home")
+    public String homePath() {
+        return "home.html";
+    }
+
+    @GetMapping("/")
+    public String redirectToHome() {
+        return "redirect:/home";
+    }
+}
---
+++ web-chat/src/main/resources/static/images/webchat v1.0_arquivos/main.js
@@ -0,0 +1,75 @@
+var getSignupBtn = document.querySelector("#signup-btn")
+var getLoginBtn = document.querySelector("#login-btn");
+var getSignupForm = document.querySelector("#singup-form").elements;
+var getLoginForm  = document.querySelector("#login-form").elements;
+var getSignupDanger  = document.querySelector("#signup-danger");
+var getLoginDanger  = document.querySelector("#login-danger");
+var getLoginDangerText  = document.querySelector("#login-danger-text");
+var getSignupDangerText  = document.querySelector("#signup-danger-text");
+
+var bearerToken = null;
+
+async function postUser(event) {
+    event.preventDefault();
+    getSignupDanger.classList.add("hidden");
+
+    const body = {
+        nickname: getSignupForm["nickname"].value,
+        email: getSignupForm["email"].value,
+        password: getSignupForm["password"].value
+    }
+
+    try {
+        const response = await fetch("/user/save", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body)
+        })
+
+        const result = await response.json();
+
+        if (result["code"] != 201) {
+            getSignupDangerText.innerHTML = result["message"];
+            getSignupDanger.classList.remove("hidden");
+        }
+    } catch(error) {
+        console.log("Error: ", error);
+    }
+}
+
+async function getUser(event) {
+    event.preventDefault();
+    getLoginDanger.classList.add("hidden");
+    
+    const body = {
+        nickname: getLoginForm["nickname"].value,
+        password: getLoginForm["password"].value
+    }
+
+    try {
+        const response = await fetch("/user/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body)
+        })
+
+        const result = await response.json();
+
+        if (result["code"] != 200) {
+            getLoginDangerText.innerHTML = result["message"];
+            getLoginDanger.classList.remove("hidden");
+        } else {
+            bearerToken = result["token"];
+        }
+    } catch(error) {
+        console.log("Error: ", error);
+    }
+}
+
+
+getSignupBtn.addEventListener("click", postUser);
+getLoginBtn.addEventListener("click", getUser);
