3af21d08f8dc64017b1371b9639561c2756fe718
rianbar <rianbarroso596@gmail.com>
2024-06-19 13:12:17 -0300

    on received message trigger added

--- web-chat/src/main/resources/static/javascript/socketCon.js
+++ web-chat/src/main/resources/static/javascript/socketCon.js
@@ -55,10 +55,53 @@ function onError(error) {
 
 
 function onMessageReceived(payload) {
+    var message = JSON.parse(payload.body);
 
+    var messageElement = document.createElement("li");
+
+    if(message.type === "JOIN") {
+        messageElement.classList.add("event-message");
+        message.content = message.sender + " joined!";
+    } else if (message.type === "LEAVE") {
+        messageElement.classList.add("event-message");
+        message.content = message.sender + " left!";
+    } else {
+        messageElement.classList.add("message-field");
+        
+        //avatar
+        var avatarElement = document.createElement("i");
+        var avatarText = document.createTextNode(message.sender[0]);
+        avatarElement.appendChild(avatarText);
+        avatarElement.style["background-color"] = getAvatarColor(message.sender);
+        messageElement.appendChild(avatarElement);
+
+        //username
+        var usernameElement = document.createElement("span");
+        var usernameText = document.createTextNode(message.sender);
+        usernameElement.appendChild(usernameText);
+        messageElement.appendChild(usernameElement);
+    }
+
+    //message
+    var textElement = document.createElement("p");
+    var messageText = document.createTextNode(message.content);
+    textElement.appendChild(messageText);
+    messageElement.appendChild(textElement);
+
+    //insert message
+    getMessageArea.appendChild(messageElement);
+    getMessageArea.scrollTop = getMessageArea.scrollHeight;
 }
 
+function getAvatarColor(messageSender) {
+    var hash = 0;
+    for (var i = 0; i < messageSender.lenght; i++) {
+        hash = 31 * hash + messageSender.charCodeAt(i);
+    }
 
+    var index = Math.abs(hash % colors.lenght);
+    return colors[index];
+}
 
 
 connectBtn.addEventListener("click", connect, true);
